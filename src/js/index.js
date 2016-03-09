1030672df4ba78bf0eb75ea8f1a18ad385f22048
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-08 22:51:30 -0500

    hitting a wall
    trying to link of javascript, thinking of how to reveal text. haven't even touched transitions.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 // Javascript Entry Point
+
+var peter = document.querySelector('.button1');
+peter.addEventListener('click', peter.onclick )
+//when I click this banner, i wanted this class to show
+var richard = document.querySelector('.button1');
+peter.addEventListener('click', peter.onclick )
+
+var oliver = document.querySelector('.button1');
+peter.addEventListener('click', peter.onclick )
+	
