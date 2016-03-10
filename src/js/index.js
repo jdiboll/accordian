7f405f1b9ad312f4da4d44d5b5a4d50fcd0fa97d
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-10 13:32:38 -0500

    getting there...

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 // Javascript Entry Point
-
-var peter = document.querySelector('.button1');
-peter.addEventListener('click', peter.onclick )
-//when I click this banner, i wanted this class to show
-var richard = document.querySelector('.button1');
-peter.addEventListener('click', peter.onclick )
-
-var oliver = document.querySelector('.button1');
-peter.addEventListener('click', peter.onclick )
+import $ from 'jquery';
 	
+
+$('.banner2').on('click', function(event) {
+	$('.slide2top').addClass('slide2bottom');
+
+	var $banner2 = $(banner2);
+	$banner2.parent().removeClass('slide2bottom');
+
+});
+
+	
+
+$('.banner3').on('click', function(event) {
+	$('.slide3top').addClass('slide3bottom');
+
+	var $banner3 = $(banner3);
+	$banner3.parent().removeClass('slide3bottom');
+});
+
