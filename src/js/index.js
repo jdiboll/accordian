9d72542e52bde5965a328f2d35cd83a012246b11
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-10 20:16:54 -0500

    simply
    simplifying the html and working on the scss...

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,13 @@
 import $ from 'jquery';
 	
 
-$('.banner2').on('click', function(event) {
-	$('.slide2top').addClass('slide2bottom');
+$('span').on('click', function(event) {
+	$('.top').addClass('bottom');
 
-	var $banner2 = $(banner2);
-	$banner2.parent().removeClass('slide2bottom');
+	var $span = $(span);
+	$span.parent().removeClass('bottom');
 
 });
 
 	
-
-$('.banner3').on('click', function(event) {
-	$('.slide3top').addClass('slide3bottom');
-
-	var $banner3 = $(banner3);
-	$banner3.parent().removeClass('slide3bottom');
-});
 
